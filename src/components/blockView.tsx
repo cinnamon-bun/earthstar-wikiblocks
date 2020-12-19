ba5d2e08c40f5cb2acae6a5be9e229eed6752e74
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-12-18 21:12:10 -0800

    edit mode for blocks

--- src/components/blockView.tsx
+++ src/components/blockView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { timestampToHuman } from '../lib/util';
 import { Block } from '../lib/wikiLayer';
 import ReactMarkdown from 'react-markdown'
@@ -9,16 +9,40 @@ export interface BlockViewProps {
     block: Block;
 }
 export let BlockView = (props: BlockViewProps) => {
+    let [editingText, setEditingText] = useState<string | null>(null);  // null means not editing
     let block = props.block;
+    let beginEditing = () => {
+        setEditingText(block.text);
+    };
+    let handleEditingChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setEditingText(e.target.value);
+    }
+    let saveEditing = () => {
+        // TODO: save to earthstar
+        setEditingText(null);
+    };
+    let cancelEditing = () => {
+        setEditingText(null);
+    };
     return (
         <div className="blockRow">
             <div className="buttonColumn">
-                <button type="button">edit</button>
+                {editingText === null
+                    ? <button type="button" className='edit' onClick={beginEditing}>edit</button>
+                    : <>
+                        <button type="button" className='save' onClick={saveEditing}>save</button>
+                        <button type="button" className='cancel' onClick={cancelEditing}>cancel</button>
+                    </>
+                }
             </div>
             <div className="blockContent">
-                <ReactMarkdown className="markdown">
-                    {block.text}
-                </ReactMarkdown>
+                {editingText === null
+                    ? <ReactMarkdown className="markdown">
+                        {block.text}
+                    </ReactMarkdown>
+                    : <textarea value={editingText} onChange={handleEditingChange} />
+                }
+                {/*
                 <div className="details">
                     {block.owner !== 'common' ? null : (
                         <div>by: {block.author.slice(0, 12) + '...'}</div>
@@ -27,6 +51,7 @@ export let BlockView = (props: BlockViewProps) => {
                     <div>created: {timestampToHuman(block.creationTimestamp)}</div>
                     <div>edited: {timestampToHuman(block.editTimestamp)}</div>
                 </div>
+                */}
             </div>
         </div>
     );
